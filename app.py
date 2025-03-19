16daee2d5be68585dc4943ea689e0693fffe47a0
FantasyBasketballWatchlist <fantasybasketballwatchlist@gmail.com>
2025-03-18 22:35:20 -0400

    Update changes

--- app.py
+++ app.py
@@ -80,7 +80,7 @@ def get_player_stats():
         return jsonify({"error": str(e)}), 400  # Handle other potential errors
 
 @app.route('/trade-analyzer')
-def trade-analyzer():
+def trade_analyzer():
     return render_template('trade-analyzer.html')
 
 # Route for fetching today's NBA scoreboard
