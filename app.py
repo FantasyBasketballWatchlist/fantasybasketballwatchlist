4a16287d49037dbbc568f0c1872ea7a8ebf39740
FantasyBasketballWatchlist <fantasybasketballwatchlist@gmail.com>
2025-04-21 22:30:38 -0400

    Quick update

--- app.py
+++ app.py
@@ -546,5 +546,5 @@ def get_player_comprehensive_data():
     return jsonify(comprehensive_data)
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 8080))
     app.run(debug=True, host='0.0.0.0', port=port)
